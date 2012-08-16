59ec948528583df74998a6c893a0eb74e6d9d30b
Callan Bryant <callan.bryant@gmail.com>
2012-08-16 17:07:53 +0100

    operate in tmpdir now

--- magic.sh
+++ magic.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # run curl https://raw.github.com/naggie/dotfiles/master/magic.sh | bash to install everything
 
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
 git clone --depth 1 --recursive git://github.com/naggie/dotfiles.git
 cd dotfiles/
 bash setup.sh
 cd ..
 rm -rf dotfiles/
+
+rmdir $TMPDIR
