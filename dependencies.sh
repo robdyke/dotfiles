e072476996fd490ab2a8afc4821594c23b821fcb
Callan Bryant <callan.bryant@gmail.com>
2012-12-04 14:17:07 +0000

    dependencies.sh and magic.sh will abort if package manager is not there

--- dependencies.sh
+++ dependencies.sh
@@ -7,14 +7,15 @@
 if [ -x /usr/bin/apt-get ]; then
 	# debian/ubuntu
 	yes | sudo apt-get update
-	yes | sudo apt-get install vim git tmux build-essential keychain mcabber htop 
+	yes | sudo apt-get install vim git tmux build-essential keychain mcabber htop
 elif [ -x /usr/local/bin/brew ]; then
 	# mac os x (vim comes out-of-the-box, yay!)
 	brew update
-	brew install git tmux keychain mcabber htop-osx
+	brew install git tmux keychain mcabber htop-osx nodejs
 else
 	echo 'Could not find a supported package manager. Install from source.'
 	echo "If you're using Arch, please add pacman support to this script"
 	echo "If you're using mac os x, install homebrew"
+	exit 1
 fi
 
