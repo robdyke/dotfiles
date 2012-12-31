920fa308829da27b7dee4034684599f1d87550a4
Callan Bryant <callan.bryant@gmail.com>
2012-12-31 17:23:14 +0000

    install motd script

---
+++ etc/install-motd.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+cd "$(dirname $0)"
+
+if [ ! -d /etc/update-motd.d/ ]; then
+	echo "update-motd system not found. Sure this is Ubuntu?"
+	exit 2
+fi
+
+if [ ! -x /usr/bin/figlet ]; then
+	echo "Install figlet first"
+fi
+
+
+# copy replacement header
+sudo cp 00-header /etc/update-motd.d/
+
+
+
+
+# remove sysinfo part. MLDASH does this.
+sudo rm /etc/update-motd.d/*-landscape-sysinfo
