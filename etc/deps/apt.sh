76bbd7efedd6a9e59a252bccec8f7800dc1b3e03
Callan Bryant <callan.bryant@gmail.com>
2013-02-06 10:38:22 +0000

    Fixes #45: switch to separate scripts for deps

---
+++ etc/deps/apt.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# debian/ubuntu
+sudo apt-get update || exit 2
+sudo apt-get install picocom vim git ssh tmux figlet build-essential bash-completion keychain mcabber htop autossh bc
+
---
+++ etc/deps/homebrew.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+brew update || exit 2
+# coreutils by default are outdated
+
+brew install git tmux keychain picocom mcabber htop-osx nodejs autossh bash-completion gnu-sed coreutils
