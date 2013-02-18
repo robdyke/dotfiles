3d81df341fa14e48e63118694e3f463996b42d1d
Callan Bryant <callan.bryant@gmail.com>
2013-02-18 10:41:59 +0000

    +git-svn

--- etc/deps/apt.sh
+++ etc/deps/apt.sh
@@ -2,5 +2,5 @@
 
 # debian/ubuntu
 sudo apt-get update || exit 2
-sudo apt-get install picocom vim git ssh tmux figlet build-essential bash-completion keychain mcabber htop autossh bc
+sudo apt-get install picocom vim git ssh tmux figlet build-essential bash-completion keychain mcabber htop autossh bc git-svn
 
