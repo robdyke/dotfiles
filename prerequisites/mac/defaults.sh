d3a8067fc0da47ca1447c8f5d6c3ce3dd636ad2c
Callan Bryant <callan.bryant@gmail.com>
2013-03-12 09:43:35 +0000

    Update defaults.sh

--- prerequisites/mac/defaults.sh
+++ prerequisites/mac/defaults.sh
@@ -16,6 +16,10 @@ defaults write com.apple.dashboard mcx-disabled -boolean YES
 # make the user library folder visible again
 chflags no hidden ~/Library/
 
+# stop preview from opening every PDF you've ever opened every time you view a PDF
+# (how did apple think this was a good idea!?!?!)
+defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -bool false
+
 # refresh stuff
 killall Dock
 killall Finder
