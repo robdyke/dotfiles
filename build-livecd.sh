0eaea2cfc23abaa2fcef63e9c20977e1be414be1
Callan Bryant <callan.bryant@gmail.com>
2014-02-16 21:15:46 +0000

    rm firefox, back to static build dir

--- build-livecd.sh
+++ build-livecd.sh
@@ -20,8 +20,6 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 UBUNTU_ISO_URL='http://www.ubuntu.com/start-download?distro=desktop&bits=64&release=latest'
 SOURCE='ubuntu-13.10-desktop-amd64.iso'
 TARGET="darkbuntu-$BRANCH.iso"
-BUILD_DIR='build'
-DOTFILES_DIR=$(dirname $0)
 
 # TODO: sort out what happens when not run in this dir
 
@@ -61,7 +59,7 @@ fi
 
 # less typing, with environment variables set
 function INSIDE {
-	chroot "$BUILD_DIR"/root \
+	chroot build/root \
 		/usr/bin/env \
 		HOME=/root \
 		LC_ALL=C \
@@ -77,11 +75,11 @@ function BREAKPOINT {
 # might fail if things are not there yet, but that's fine.
 function EMERGENCY_CLEANUP {
 	echo Emergency cleanup...
-	umount -l "$BUILD_DIR"/root/proc
-	umount    "$BUILD_DIR"/root/sys
-	umount -l "$BUILD_DIR"/root/dev
-	umount    "$BUILD_DIR"/root/dev/pts
-	umount    "$BUILD_DIR"/mnt
+	umount -l build/root/proc
+	umount    build/root/sys
+	umount -l build/root/dev
+	umount    build/root/dev/pts
+	umount    build/mnt
 	exit
 }
 
@@ -91,29 +89,29 @@ trap EMERGENCY_CLEANUP SIGINT
 # DEBUG
 #set -x
 
-[ -d "$BUILD_DIR" ] || mkdir "$BUILD_DIR"/
+[ -d build ] || mkdir build/
 
 echo; echo; echo
 set -x
 
 # clean
-rm -rf "$BUILD_DIR"/*
+rm -rf build/*
 
-mkdir "$BUILD_DIR"/mnt
-mount -o loop,ro "$SOURCE" "$BUILD_DIR"/mnt
+mkdir build/mnt
+mount -o loop,ro "$SOURCE" build/mnt
 
 # extract ISO so files are writable
-mkdir "$BUILD_DIR"/extract
-rsync --exclude=/casper/filesystem.squashfs -a "$BUILD_DIR"/mnt/ "$BUILD_DIR"/extract
+mkdir build/extract
+rsync --exclude=/casper/filesystem.squashfs -a build/mnt/ build/extract
 
 # Extract the Desktop system
 # Extract the SquashFS filesystem
-unsquashfs -no-progress -d "$BUILD_DIR"/root "$BUILD_DIR"/mnt/casper/filesystem.squashfs
+unsquashfs -no-progress -d build/root build/mnt/casper/filesystem.squashfs
 
 # Prepare and chroot
 # network connection within chroot
-cp /etc/resolv.conf "$BUILD_DIR"/root/etc/
-cp /etc/hosts       "$BUILD_DIR"/root/etc/
+cp /etc/resolv.conf build/root/etc/
+cp /etc/hosts       build/root/etc/
 
 # other filesystems, inside chroot
 # these mount important directories of your host system - if you later decide to
@@ -121,11 +119,12 @@ cp /etc/hosts       "$BUILD_DIR"/root/etc/
 # otherwise your host system will become unusable at least temporarily until
 # reboot)
 # Also rm -rf'ing over binded dev really isn't a good thing...
-mount -t proc   none "$BUILD_DIR"/root/proc
-mount -t sysfs  none "$BUILD_DIR"/root/sys
-mount -t devpts none "$BUILD_DIR"/root/dev/pts
-mount --bind /dev/   "$BUILD_DIR"/root/dev
+mount -t proc   none build/root/proc
+mount -t sysfs  none build/root/sys
+mount -t devpts none build/root/dev/pts
+mount --bind /dev/   build/root/dev
 
+# TODO: move some of these apt commands to the provision script, or a custom one
 # In 9.10, (+?) before installing or upgrading packages you need to run
 # also may as well update/upgrade and add repositories
 dbus-uuidgen | INSIDE tee /var/lib/dbus/machine-id
@@ -140,18 +139,20 @@ yes | INSIDE apt-get install git
 yes | INSIDE apt-get remove --purge 'libreoffice*'
 yes | INSIDE apt-get upgrade
 
+yes | apt-get install broadcom-sta-common
+
 #BREAKPOINT
 
 # install packages
 # and dotfiles
 # naggie/dotfiles does this all
 # installs dotfiles to /etc/skel/ so that live (ubuntu) user will get a
-#cp -a ../dotfiles "$BUILD_DIR"/root/root/
-#git clone . "$BUILD_DIR"/root/root/dotfiles
+#cp -a ../dotfiles build/root/root/
+#git clone . build/root/root/dotfiles
 # rsync preserves original origin and submodules, but git submodules have
 # absolute references which break if you move the git folder on old versions of
 # git...
-#rsync -r --exclude="$BUILD_DIR" --exclude='*iso' "$DOTFILES_DIR" "$BUILD_DIR"/root/root/dotfiles
+#rsync -r --exclude=build --exclude='*iso' "$DOTFILES_DIR" build/root/root/dotfiles
 INSIDE /bin/bash -x <<EOF
 	git clone git://github.com/naggie/dotfiles.git
 	cd dotfiles
@@ -175,56 +176,56 @@ INSIDE apt-get clean
 INSIDE apt-get autoremove
 
 
-rm -rf "$BUILD_DIR"/root/tmp/*
-rm     "$BUILD_DIR"/root/.bash_history
+rm -rf build/root/tmp/*
+rm     build/root/.bash_history
 
-rm "$BUILD_DIR"/root/etc/hosts
-rm "$BUILD_DIR"/root/etc/resolv.conf
+rm build/root/etc/hosts
+rm build/root/etc/resolv.conf
 
 # Clean after installing software
-rm "$BUILD_DIR"/root/var/lib/dbus/machine-id
-rm "$BUILD_DIR"/root/sbin/initctl
+rm build/root/var/lib/dbus/machine-id
+rm build/root/sbin/initctl
 INSIDE dpkg-divert --rename --remove /sbin/initctl
 
 # now umount (unmount) special filesystems before creation of iso
-umount -l "$BUILD_DIR"/root/proc
-umount    "$BUILD_DIR"/root/sys
-umount -l "$BUILD_DIR"/root/dev
-umount    "$BUILD_DIR"/root/dev/pts
-umount    "$BUILD_DIR"/mnt
+umount -l build/root/proc
+umount    build/root/sys
+umount -l build/root/dev
+umount    build/root/dev/pts
+umount    build/mnt
 
 # ASSEMBLE ISO
-chmod +w "$BUILD_DIR"/extract/casper/filesystem.manifest
+chmod +w build/extract/casper/filesystem.manifest
 
-INSIDE dpkg-query -W --showformat='${Package} ${Version}\n' > "$BUILD_DIR"/extract/casper/filesystem.manifest
+INSIDE dpkg-query -W --showformat='${Package} ${Version}\n' > build/extract/casper/filesystem.manifest
 
-cp "$BUILD_DIR"/extract/casper/filesystem.manifest "$BUILD_DIR"/extract/casper/filesystem.manifest-desktop
+cp build/extract/casper/filesystem.manifest build/extract/casper/filesystem.manifest-desktop
 
-sed -i '/ubiquity/d' "$BUILD_DIR"/extract/casper/filesystem.manifest-desktop
-sed -i '/casper/d'   "$BUILD_DIR"/extract/casper/filesystem.manifest-desktop
+sed -i '/ubiquity/d' build/extract/casper/filesystem.manifest-desktop
+sed -i '/casper/d'   build/extract/casper/filesystem.manifest-desktop
 
 # Compress filesystem
 # already excluded by rsync
-#rm "$BUILD_DIR"/extract/casper/filesystem.squashfs
+#rm build/extract/casper/filesystem.squashfs
 
 # For a highest possible compression at the cost of compression time, you may
 # use the xz method and is better exclude the edit/boot directory altogether:
-#mksquashfs "$BUILD_DIR"/root/ "$BUILD_DIR"/extract/casper/filesystem.squashfs
+#mksquashfs build/root/ build/extract/casper/filesystem.squashfs
 mksquashfs \
-	"$BUILD_DIR"/root "$BUILD_DIR"/extract/casper/filesystem.squashfs \
-	-comp xz -e "$BUILD_DIR"/root/boot -no-progress
+	build/root build/extract/casper/filesystem.squashfs \
+	-comp xz -e build/root/boot -no-progress
 
 # Update the filesystem.size file, which is needed by the installer:
-printf $(du -sx --block-size=1 "$BUILD_DIR"/root | cut -f1) > "$BUILD_DIR"/extract/casper/filesystem.size
+printf $(du -sx --block-size=1 build/root | cut -f1) > build/extract/casper/filesystem.size
 
 # Set an image name in extract-cd/README.diskdefines
 #vim extract-cd/README.diskdefines
 
 # recalc hashes
-rm "$BUILD_DIR"/extract/md5sum.txt
+rm build/extract/md5sum.txt
 # subshell, no chdir persistence
 (
-	cd "$BUILD_DIR"/extract
+	cd build/extract
 	find -type f -print0 \
 		| xargs -0 md5sum \
 		| grep -v isolinux/boot.cat \
@@ -237,13 +238,13 @@ mkisofs -D -r -V "Darkbuntu" -cache-inodes -J -l \
 	-c isolinux/boot.cat \
 	-no-emul-boot -boot-load-size 4 \
 	-boot-info-table \
-	-o "$TARGET" "$BUILD_DIR"/extract/
+	-o "$TARGET" build/extract/
 
 # clean, TODO MUST MAKE SURE EVERYTHING IS UNMOUNTED FIRST, PARTICULARLY /dev
-rm -rf "$BUILD_DIR"/*
+rm -rf build/*
 
 # yay git
-#touch "$BUILD_DIR"/.empty
+#touch build/.empty
 
 # postprocess to allow simple dd to flash drive to work?
 # isohybrid
