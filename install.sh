be5a16261b6b463cb5d17f6473d6d4df95df3c72
Callan Bryant <callan.bryant@gmail.com>
2016-05-20 00:08:44 +0100

    remove old dangling ctrlp submod

--- install.sh
+++ install.sh
@@ -52,6 +52,7 @@ git submodule --quiet update || exit 2
 # Garbage collection
 # remove remains of old submodules, scripts, etc
 rm -rf home/.vim/bundle/powerline 2>/dev/null
+rm -rf home/.vim/bundle/ctrlp     2>/dev/null
 rm -f  ~/bin/server-splash.sh     2>/dev/null
 rm -f  ~/git-completion.bash      2>/dev/null
 
