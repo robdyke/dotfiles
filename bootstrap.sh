9af88f08a599fb89f1707da03dc6c469ca531ee4
Callan Bryant <callan.bryant@gmail.com>
2017-09-17 11:35:29 +0100

    trust github and use alternate method for bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
-# curl -sL https://github.com/naggie/dotfiles/raw/naggie/bootstrap.sh | bash
+# curl -sL https://github.com/naggie/dotfiles/raw/naggie/bootstrap.sh | bash && bash
 
 cat <<EOF
-This script will install vim/git/tmux and then dotfiles.
+This script will install vim/git/tmux and then dotfiles, trusting github public key.
 Hit CTRL+C to abort in the next 3 seconds....
 
 EOF
 
 sleep 3
 
+
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+    chmod 700 ~/.ssh
+fi
+
+touch ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
+
+test grep github.com ~/.ssh/known_hosts || cat <<EOF >> ~/.ssh/known_hosts
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+EOF
 
 if [ $(uname) == 'Darwin' ]; then
     # macos
@@ -37,6 +49,3 @@ git clone -b naggie git@github.com:naggie/dotfiles.git || git clone https://gith
 
 cd dotfiles
 ./install.sh
-
-# start tmux (with reloaded shell)
-tmux attach || tmux
