a345ad1ec4b728a1801228f8c4af04c2ea78550b
Callan Bryant <callan.bryant@gmail.com>
2018-10-07 23:30:04 +0100

    missing dot from path to .local/bin

--- home/.env.sh
+++ home/.env.sh
@@ -1,6 +1,6 @@
 # common env. Fish needs an export polyfil. No dynamic stuff here (thanks, fish)
 export GOPATH=~/gocode
-export PATH=~/local/bin:~/bin:/usr/local/bin:/snap/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:/usr/local/go/bin:$PATH
+export PATH=~/.local/bin:~/bin:/usr/local/bin:/snap/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:/usr/local/go/bin:$PATH
 export EDITOR=vim
 export PAGER='less -R'
 export LANG=en_GB.UTF-8
