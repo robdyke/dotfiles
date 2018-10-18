21cdcc1e5f48a9d2393de60976bb1a0867775bb9
Callan Bryant <callan.bryant@gmail.com>
2018-10-18 23:01:16 +0100

    fix expansion of homedir in aliases and env
    fixes anything that uses ripgrep

--- home/.env.sh
+++ home/.env.sh
@@ -1,18 +1,18 @@
 # common static env here
-export GOPATH=~/gocode
-export PATH=~/.local/bin:~/bin:/usr/local/bin:/snap/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:/usr/local/go/bin:/usr/local/opt/coreutils/libexec/gnubin:$PATH
+export HOME=~
+export GOPATH=$HOME/gocode
+export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:/snap/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:/usr/local/go/bin:/usr/local/opt/coreutils/libexec/gnubin:$PATH
 export EDITOR=_vim
 export PAGER="less -R"
 export LANG=en_GB.UTF-8
-export BC_ENV_ARGS="~/.bcrc -l"
+export BC_ENV_ARGS="$HOME/.bcrc -l"
 export GCC_COLORS=1
-# note that ~ does not expand in .ripgreprc
-export FZF_DEFAULT_COMMAND="rg --smart-case --files --hidden --ignore-file=~/.rgignore"
-export FZF_CTRL_T_COMMAND="rg --smart-case --files --hidden --ignore-file=~/.rgignore"
+export FZF_DEFAULT_COMMAND="rg --smart-case --files --hidden --ignore-file=$HOME/.rgignore"
+export FZF_CTRL_T_COMMAND="rg --smart-case --files --hidden --ignore-file=$HOME/.rgignore"
 
 # Sometimes not set or fully qualified; simple name preferred.
 export HOSTNAME=$(hostname -s)
-export SYSTEM_COLOUR=$(~/.local/bin/system-colour $HOSTNAME)
+export SYSTEM_COLOUR=$($HOME/.local/bin/system-colour $HOSTNAME)
 
 if [[ $USER == root ]]; then
     PROMPT_COLOUR=160 # red
