02121e05187e68fa206cbeea29f1ba4df5b0130f
Callan Bryant <callan.bryant@gmail.com>
2018-10-21 18:26:59 +0100

    separate platform detection

--- provision.sh
+++ provision.sh
@@ -17,14 +17,31 @@ EOF
 
 sleep 3
 
+
+# platform detection. Desktop is always a superset of server.
+if [ $(uname) == 'Darwin' ]; then
+    export MACOS_DESKTOP=1
+elif grep -q Ubuntu /etc/issue; then
+    if dpkg -l | grep -q xorg-server; then
+        export UBUNTU_DESKTOP=1
+    else
+        export UBUNTU_SERVER=1
+    fi
+elif grep -q Raspbian /etc/issue; then
+    export RASPBIAN_SERVER=1
+else
+    echo "Unsupported OS."
+    exit 2
+fi
+
+
 # tmpreaper tries to do a post-install "configuration" screen to warn the user.
 export DEBIAN_FRONTEND=noninteractive
 
 # packages are installed via the stock system package manager unless a more
 # recent version is available elsewhere.
 
-if [ $(uname) == 'Darwin' ]; then
-    # macos
+if [ $MACOS_DESKTOP ]; then
     if [ ! -f /usr/local/bin/brew ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     else
@@ -49,21 +66,24 @@ if [ $(uname) == 'Darwin' ]; then
 
     # correct so alias works cross platform
     ln -sf /usr/local/bin/gpg /usr/local/bin/gpg2
+fi
 
-elif grep -q Ubuntu /etc/issue || grep -q Raspbian /etc/issue; then
-    if grep -q Ubuntu /etc/issue; then
-        sudo -E apt-add-repository multiverse
-        sudo -E apt-get -y install language-pack-en
-        sudo -E apt-get -y install curl
-        # FYI -- ripgrep is an official 18.04 package
-        curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb > /tmp/ripgrep.deb
-        sudo dpkg -i /tmp/ripgrep.deb
-        rm /tmp/ripgrep.deb
-    else
-        curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep-0.10.0-arm-unknown-linux-gnueabihf.tar.gz \
-            | sudo tar -C /usr/local/bin --strip=1 -xzf - ripgrep-0.10.0-arm-unknown-linux-gnueabihf/rg
-    fi
+if [ $UBUNTU_SERVER ] || [ $UBUNTU_DESKTOP ]; then
+    sudo -E apt-add-repository multiverse
+    sudo -E apt-get -y install language-pack-en
+    sudo -E apt-get -y install curl
+    # FYI -- ripgrep is an official 18.04 package
+    curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb > /tmp/ripgrep.deb
+    sudo dpkg -i /tmp/ripgrep.deb
+    rm /tmp/ripgrep.deb
+fi
 
+if [ $RASPBIAN_SERVER ]; then
+    curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep-0.10.0-arm-unknown-linux-gnueabihf.tar.gz \
+        | sudo tar -C /usr/local/bin --strip=1 -xzf - ripgrep-0.10.0-arm-unknown-linux-gnueabihf/rg
+fi
+
+if [ $UBUNTU_DESKTOP ] || [ $UBUNTU_SERVER ] || [ $RASPBIAN_SERVER ]; then
     sudo -E apt-get -y update
     sudo -E apt-get -y install tmux vim git tig zsh ssh figlet httpie ncdu tree wget htop gnupg2 curl keychain tmpreaper bash-completion \
         jq sox ffmpeg httrack python python3 golang libffi-dev python-pip python3-pip python-dev python3-dev libssl-dev dconf-cli
@@ -72,9 +92,6 @@ elif grep -q Ubuntu /etc/issue || grep -q Raspbian /etc/issue; then
     sudo python3 -m pip install --upgrade ansible ipython
 
     sudo -E ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-else
-    echo "Unsupported OS."
-    exit 2
 fi
 
 
