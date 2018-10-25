43238844403dff6bbe4c3786a7bc8df297223e72
Callan Bryant <callan.bryant@gmail.com>
2018-10-25 22:30:08 +0100

    indent keyboard settings with leading whitespace filter

--- provision.sh
+++ provision.sh
@@ -82,12 +82,12 @@ if [ $UBUNTU ] || [ $RASPBIAN ]; then
     sudo -E ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 
     # set keyboard layout (with caps lock as esc)
-    cat <<'EOF' | sudo tee /etc/default/keyboard
-XKBMODEL="pc104"
-XKBLAYOUT="gb"
-XKBVARIANT=""
-XKBOPTIONS="caps:escape"
-BACKSPACE="guess"
+    cat <<- EOF | sudo tee /etc/default/keyboard
+        XKBMODEL="pc104"
+        XKBLAYOUT="gb"
+        XKBVARIANT=""
+        XKBOPTIONS="caps:escape"
+        BACKSPACE="guess"
 EOF
     # apply temporarily, as above requires restart (I think)
     test $DISPLAY && setxkbmap -option "caps:escape" gb
