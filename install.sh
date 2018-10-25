0b9d9cb7b56bf2d4e1fee5bd415ab7862ddab8a0
Callan Bryant <callan.bryant@gmail.com>
2018-10-25 18:34:55 +0100

    only set .history perms on creation

--- install.sh
+++ install.sh
@@ -91,8 +91,8 @@ fi
 
 if [ -f ~/.bash_history ] && [ ! -f ~/.history ]; then
     cp ~/.bash_history ~/.history
+    chmod 600 ~/.history
 fi
-chmod 600 ~/.history
 
 # restore task context (maybe)
 test $TASK_CONTEXT && task context $TASK_CONTEXT &>/dev/null
