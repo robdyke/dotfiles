a6421eb9fdcb7910d113d39f65be3c498b549dd3
Callan Bryant <callan.bryant@gmail.com>
2019-01-03 20:54:20 +0000

    remove figlet

--- provision.sh
+++ provision.sh
@@ -195,7 +195,7 @@ fi
 
 if [ $UBUNTU ] || [ $RASPBIAN ]; then
     sudo -E apt-get -y update
-    sudo -E apt-get -y install tmux vim git tig zsh ssh pass figlet httpie ncdu tree wget htop gnupg2 curl keychain tmpreaper bash-completion \
+    sudo -E apt-get -y install tmux vim git tig zsh ssh pass httpie ncdu tree wget htop gnupg2 curl keychain tmpreaper bash-completion \
         jq sox ffmpeg httrack python python3 golang libffi-dev python-pip python3-pip python-dev python3-dev libssl-dev dconf-cli scdaemon \
         pcscd rxvt-unicode-256color
 
