0db193207f17d182ba6159a601faba0f5bbea4f2
Callan Bryant <callan.bryant@gmail.com>
2019-01-09 18:09:28 +0000

    install nvim, check hash of existing file on dl

--- provision.sh
+++ provision.sh
@@ -59,7 +59,7 @@ function download_to_cache_dir {
     FILENAME="$(echo $URL | rev | cut -d / -f 1 | rev)"
     TARGET="${DIR}/${FILENAME}"
 
-    if [ ! -f "$TARGET" ]; then
+    if [ ! -f "$TARGET" ] || ! sha256sum $TARGET | grep -q $SHA256SUM; then
         curl -L "$URL" > "$TARGET"
     fi
 
@@ -135,9 +135,9 @@ if [ $UBUNTU ]; then
     tar -C $BIN_DIR xzf "${CACHE_DIR}/Alacritty-v0.2.4-x86_64.tar.gz"
 
     # neovim (don't write directly, swap atomically so running nvim won't block)
-    curl -L https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage > $PREFIX/nvim.new
-    chmod +x $PREFIX/nvim.new
-    mv $PREFIX/nvim.new $PREFIX/nvim
+    download_to_cache_dir https://github.com/neovim/neovim/releases/download/v0.3.3/nvim.appimage 6c937c0a2b37e4ad99bae2f37f461ae47a590e62bddecf903b0b5bafe0eaaadb
+    chmod +x "${CACHE_DIR}/nvim.appimage"
+    mv +x "${CACHE_DIR}/nvim.appimage" "${BIN_DIR}/"
 fi
 
 if [ $RASPBIAN ]; then
