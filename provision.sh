1f91186355b75c32bc09f5c29c9a421c9150cb5b
Callan Bryant <callan.bryant@gmail.com>
2019-01-12 09:19:29 +0000

    correct fzf untar cmd

--- provision.sh
+++ provision.sh
@@ -151,7 +151,7 @@ if [ $RASPBIAN ]; then
 
     # FZF
     download_to_cache_dir https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_arm5.tgz aa5f46c21fb765a20494fdcb00b86b6bde3b29538f25d49398f3b423f8e1e394
-    sudo tar -C $BIN_DIR --strip=1 -xzf "${CACHE_DIR}/fzf-0.17.5-linux_arm5.tgz" fzf
+    sudo tar -C $BIN_DIR -xzf "${CACHE_DIR}/fzf-0.17.5-linux_arm5.tgz"
 fi
 
 if [ $UBUNTU ] || [ $RASPBIAN ]; then
