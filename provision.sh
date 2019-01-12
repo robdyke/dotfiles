f36a1a9d0665a1ff7fb2d771a7cebf274b289084
Callan Bryant <callan.bryant@gmail.com>
2019-01-12 08:58:32 +0000

    correct fzf checksum for pi

--- provision.sh
+++ provision.sh
@@ -150,7 +150,7 @@ if [ $RASPBIAN ]; then
     sudo tar -C $BIN_DIR --strip=1 -xzf "${CACHE_DIR}/ripgrep-0.10.0-arm-unknown-linux-gnueabihf.tar.gz" ripgrep-0.10.0-arm-unknown-linux-gnueabihf/rg
 
     # FZF
-    download_to_cache_dir https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_arm5.tgz https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_arm5.tgz
+    download_to_cache_dir https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_arm5.tgz aa5f46c21fb765a20494fdcb00b86b6bde3b29538f25d49398f3b423f8e1e394
     sudo tar -C $BIN_DIR --strip=1 -xzf "${CACHE_DIR}/fzf-0.17.5-linux_arm5.tgz" fzf
 fi
 
