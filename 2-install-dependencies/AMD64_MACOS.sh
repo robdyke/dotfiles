3d1a46153c53a2906c0eafda14823d17a7084d55
Callan Bryant <callan.bryant@gmail.com>
2019-04-01 22:12:01 +0100

    osx: complete move to obtain fn

--- 2-install-dependencies/AMD64_MACOS.sh
+++ 2-install-dependencies/AMD64_MACOS.sh
@@ -15,13 +15,18 @@ brew cask install spectacle firefox alacritty
 # flux is no longer required -- night shift!
 
 # Upgrade or install (logic necessary)
-packages=(tmux vim git tig httpie ncdu tree bash pass zsh openssh jq wget htop gnupg2 bash-completion keychain iproute2mac tmpreaper \
-    coreutils sox ffmpeg httrack python ripgrep python go nvim fzf pinentry-mac ykman browserpass)
+packages=(tmux vim git tig httpie ncdu tree bash pass zsh openssh jq wget htop gnupg2 bash-completion iproute2mac tmpreaper coreutils sox \
+    ffmpeg httrack python ripgrep python go nvim fzf pinentry-mac ykman browserpass)
 for package in "${packages[@]}"; do
     brew upgrade $package || brew install $package
 done
 
 # dstask
-download_to_cache_dir https://github.com/naggie/dstask/releases/download/v0.8/dstask-darwin-amd64 08239f5cd9f56053e3b0ddc09d0e2b1b40083964810edd22161c25c6e09f12b8
-chmod +x "${CACHE_DIR}/dstask-darwin-amd64"
-sudo mv "${CACHE_DIR}/dstask-darwin-amd64" /usr/local/bin/dstask
+EXE="$(
+    obtain \
+        https://github.com/naggie/dstask/releases/download/v0.8/dstask-darwin-amd64 \
+        08239f5cd9f56053e3b0ddc09d0e2b1b40083964810edd22161c25c6e09f12b8
+)"
+sudo rm /usr/local/bin/dstask
+sudo cp "${EXE}" /usr/local/bin/dstask
+sudo chmod +x /usr/local/bin/dstask
