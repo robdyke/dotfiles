e5245343eaface0137587e003f8479497fc08772
Callan Bryant <callan.bryant@gmail.com>
2019-04-01 20:23:32 +0100

    change to echoing path in cache dir

--- 2-install-dependencies/include/util.sh
+++ 2-install-dependencies/include/util.sh
@@ -1,10 +1,4 @@
-# where to install bare EXEs (or softare compile from src)
-BIN_DIR=/usr/local/bin
 CACHE_DIR=~/.cache/dotfiles
-mkdir -p $CACHE_DIR
-# for local installs -- scripts, added to PATH with env.sh
-mkdir -p ~/.local/bin
-mkdir -p ~/.local/src
 
 # download file if it doesn't exist, then check the sha256sum matches to
 # mitigate forward-looking supply chain attacks. Exit script on fail.
@@ -15,12 +9,16 @@ function download_to_cache_dir {
     FILENAME="$(echo $URL | rev | cut -d / -f 1 | rev)"
     TARGET="${CACHE_DIR}/${FILENAME}"
 
+    mkdir -p "${CACHE_DIR}"
+
     if [ ! -f "$TARGET" ] || ! sha256sum $TARGET | grep -q $SHA256SUM; then
         curl -L "$URL" > "$TARGET"
     fi
 
     if ! sha256sum $TARGET | grep -q $SHA256SUM; then
-        echo "Corrupt or compromised download detected! See $TARGET"
+        >&2 echo "Corrupt or compromised download detected! See $TARGET"
         exit 4
     fi
+
+    echo "${TARGET}"
 }
