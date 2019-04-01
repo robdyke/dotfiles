511c285faf064c496e4a27435ace81eab7eef2f9
Callan Bryant <callan.bryant@gmail.com>
2019-04-01 20:33:55 +0100

    change to CAS based cache

--- 2-install-dependencies/include/util.sh
+++ 2-install-dependencies/include/util.sh
@@ -1,24 +1,28 @@
-CACHE_DIR=~/.cache/dotfiles
+CACHE_DIR=~/.cache/dotfiles/
 
 # download file if it doesn't exist, then check the sha256sum matches to
 # mitigate forward-looking supply chain attacks. Exit script on fail.
 function download_to_cache_dir {
     URL="$1"
     SHA256SUM="$2"
-
-    FILENAME="$(echo $URL | rev | cut -d / -f 1 | rev)"
-    TARGET="${CACHE_DIR}/${FILENAME}"
+    TARGET="${CACHE_DIR}/${SHA256SUM}.bin"
 
     mkdir -p "${CACHE_DIR}"
 
-    if [ ! -f "$TARGET" ] || ! sha256sum $TARGET | grep -q $SHA256SUM; then
-        curl -L "$URL" > "$TARGET"
+    # download if does not exist or corrupt
+    if [ ! -f $TARGET ] || ! sha256sum $TARGET | grep -q $SHA256SUM; then
+        # file may be read only, should be (over)writable
+        [ -f $TARGET ] && chmod 0600 $TARGET
+        curl -L "$URL" > $TARGET
     fi
 
     if ! sha256sum $TARGET | grep -q $SHA256SUM; then
-        >&2 echo "Corrupt or compromised download detected! See $TARGET"
+        >&2 echo "Corrupt or compromised download detected! See $TARGET ostensibly from $URL"
         exit 4
     fi
 
-    echo "${TARGET}"
+    # you will have to copy it and set correct permissions.
+    chmod 0444 $TARGET
+
+    echo $TARGET
 }
