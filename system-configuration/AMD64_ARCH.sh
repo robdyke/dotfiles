874cb269204507215e7b3f1a0ae43e92b4a9a791
Callan Bryant <callan.bryant@gmail.com>
2019-04-02 23:29:52 +0100

    set locale in arch

--- system-configuration/AMD64_ARCH.sh
+++ system-configuration/AMD64_ARCH.sh
@@ -1,1 +1,6 @@
 sudo ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+sudo hwclock --systohc
+echo 'en_GB.UTF-8 UTF-8' | sudo tee /etc/locale.gen
+sudo locale-gen
+
+# TODO hostname, somehow. Maybe this is set by the installer script.
