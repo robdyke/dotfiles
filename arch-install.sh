ce4b839585d7d5bf19c0410a80c85af464fb35c6
Callan Bryant <callan.bryant@gmail.com>
2019-04-16 19:48:25 +0100

    prompt user for install info

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-# usage
+read -e -p "Target device: " device
+
+if [ ! -b $device ]; then
+    >&2 echo "$device is not a block device"
+    exit 1
+fi
+
+read -e -p "Hostname: " hostname
+read -e -p "Username: " username
+read -e -s -p "User / FDE Password: " passwd
+
 # partition disk
+
+# TODO commands piped into fdisk or sfdisk which is a scripted fdisk
+# https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
+# https://en.wikipedia.org/wiki/Partition_type
+
 # mount partitions
 # generate fstab
 # set hostname
