cb725c1741e6143dd0b4377754580b66a725e77c
Callan Bryant <callan.bryant@gmail.com>
2020-02-12 19:48:19 +0000

    only ask for sudo password if required

--- provision.sh
+++ provision.sh
@@ -20,14 +20,15 @@ FEDORA_AMD64=FEDORA_AMD64
 RASPBIAN_ARMV5=RASPBIAN_ARMV5
 
 # authorise sudo early on
-echo "Please enter sudo password. Sudo session will be kept alive until this script exits."
-# sudo -v is technically correct but asks for a non-existent password on fresh AWS Ubuntu AMIs
-sudo echo -n
+if ! sudo -n echo 2>/dev/null; then
+    echo "Please enter sudo password. Sudo session will be kept alive until this script exits."
+    # sudo -v is technically correct but asks for a non-existent password on fresh AWS Ubuntu AMIs
+    sudo echo -n
+fi
 
 # sudo keepalive. This will keep the sudo watchdog fed until this script exits.
 # This works buy poking the parent process to see if it's still alive.
 while true; do sudo -n true; sleep 15; kill -0 "$$" || exit; done 2>/dev/null &
-
 
 function get_platform() {
     if uname | grep -q Linux; then
