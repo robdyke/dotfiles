fe303cdbb592a85a08f87e3fb8ad4e1755c228fd
Callan Bryant <callan.bryant@gmail.com>
2020-05-03 11:50:04 +0100

    turn light on for webcam automatically

--- etc/lights/install.sh
+++ etc/lights/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
 mkdir -p $HOME/.local/share/systemd/user
+
 cp locklights.service $HOME/.local/share/systemd/user/
 systemctl --user enable locklights.service
 systemctl --user start locklights.service
+
+cp camlight.service $HOME/.local/share/systemd/user/
+systemctl --user enable camlight.service
+systemctl --user start camlight.service
