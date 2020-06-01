e68eca65d754d4539408f0e3e3403bc6d6e9f6ba
Callan Bryant <callan.bryant@gmail.com>
2020-06-01 19:36:37 +0100

    dspa announce on cam on/off

--- etc/lights/install.sh
+++ etc/lights/install.sh
@@ -4,8 +4,8 @@ mkdir -p $HOME/.local/share/systemd/user
 
 cp locklights.service $HOME/.local/share/systemd/user/
 systemctl --user enable locklights.service
-systemctl --user start locklights.service
+systemctl --user restart locklights.service
 
 cp camlight.service $HOME/.local/share/systemd/user/
 systemctl --user enable camlight.service
-systemctl --user start camlight.service
+systemctl --user restart camlight.service
