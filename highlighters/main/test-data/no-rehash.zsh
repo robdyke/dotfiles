5153f922061a2dc7287bcbbd62cfcc45b09d1cf2
Callan Bryant <callan.bryant@gmail.com>
2020-08-28 07:56:35 +0100

    Squashed 'home/.zsh/zsh-syntax-highlighting/' changes from 91d2eeaf2..62c557584
    62c557584 tests: Add a test that 'rehash' isn't run.
    4cf464f84 docs: Update Gentoo link
    6d5372a93 tests: parameter-to-global-alias: Fix a false positive failure when 'x' happens to be a valid external command name.
    e9cad1493 changelog: Document #712 under the last tag, 0.8.0-alpha1-pre-redrawhook.
    79b6e7e05 tests: precommand-killing1: Use a dummy path that always exists
    3944a44ff driver: Fix a version number check to work around a bug in the zsh version whereunder the check should return false.
    34674d786 docs: Add OpenBSD port
    e4d1db1e2 docs: Sort INSTALL.md
    c14fcad3b empty commit: Close redrawhook bugs
    239c720de Merge branch 'feature/redrawhook'
    7cc622647 docs: Track making the new codepath conditional upon the 'memo=' feature.
    cba4a1bc2 On the feature/redrawhook branch, changelog: Add entries for issues fixed by this branch.
    637e1c702 Merge remote-tracking branch 'origin/master' into feature/redrawhook
    2d60a47cc Post-release version number bump.
    00a5fd11e Tag version 0.8.0-alpha1-pre-redrawhook.
    cdd7f899c brackets: Optimize the character iteration
    cb33cc008 On the feature/redrawhook branch, change the detection of the 'memo=' feature to avoid a catch-22.
    9ce3540d7 Merge remote-tracking branch 'origin/master' into feature/redrawhook
    d62baa50f Revert "driver: Move the initialization of $zsh_highlight__memo_feature out of the entry point function."
    59cb9a560 driver: Make the redrawhook codepath conditional upon the memo= feature.
    7b863fb41 Merge remote-tracking branch 'origin/master' into feature/redrawhook
    8a1bd7c8b driver: Move the initialization of $zsh_highlight__memo_feature out of the entry point function.
    daf0d94ba On the feature/redrawhook branch, move the changelog entry to the current release's section.
    c28312b9d Merge remote-tracking branch 'origin/master' into feature/redrawhook
    d9a796397 driver: Follow-up to grandparent: Have all test suite entry points declare the mock $region_highlight.
    810c2dced Use the new, unreleased zsh 'memo=' feature to remove only our own entries from $region_highlight.
    075c85292 driver: Stop re-declaring $region_highlight.  It's unneeded.
    fb929edc3 docs: regexp highlighter: Fix a wrong associative array name in the example.
    b08d508cd driver: Fix a bug that prevented subsequent, third-party zle-line-pre-redraw hooks from running.
    8d4c6355e driver: Do not pass widget arguments to _zsh_highlight
    56ba7f082 driver: Clarify comment.  No functional change.
    2cbb3fb24 driver: Allow for -U in autoloaded function definition
    f265ef0b9 driver: Use idiomatic module check
    d0fb0df4f driver: Make the shadowing $WIDGET read only.
    f665eec23 driver: Avoid a fork in the common case.
    a868b6942 test harness: Actually test the new code.
    b5249f17a driver: Rewrite without a state variable
    d2594c115 noop: Make a whitespace-only change to reduce noise in the next commit.
    66ae59ecc docs: Rewrap.
    1651137f5 docs: Update FAQ answer per changes on this branch.
    d4ab7e51d redo _zsh_highlight__function_callable_p
    38477f2a3 driver: Use a different way of checking whether add-zle-hook-widget is present.
    d98622dcd changelog: Use a more specific link.
    04fd6bbf5 changelog: Note the effect of fixing #245/#90 and an alternative.
    30c6e7039 driver: Pass zle-line-finish arguments on to _zsh_highlight.
    74a27de70 driver: Hook zle-line-finish.
    85e62a817 driver: Reimplement using 'add-zle-hook-widget zle-line-pre-redraw'
    aed99f6a3 wrappers: Reimplement using Mikachu's zle-line-pre-redraw hook (workers/36650).
    
    git-subtree-dir: home/.zsh/zsh-syntax-highlighting
    git-subtree-split: 62c5575848f1f0a96161243d18497c247c9f52df

--- highlighters/main/test-data/no-rehash.zsh
+++ highlighters/main/test-data/no-rehash.zsh
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------------------------------
-# Copyright (c) 2010-2016 zsh-syntax-highlighting contributors
+# Copyright (c) 2020 zsh-syntax-highlighting contributors
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted
@@ -27,36 +27,13 @@
 # vim: ft=zsh sw=2 ts=2 et
 # -------------------------------------------------------------------------------------------------
 
+hash zsyh-hashed-command=/usr/bin/env
+BUFFER='doesnotexist; zsyh-hashed-command'
 
-# List of keyword and color pairs.
-typeset -gA ZSH_HIGHLIGHT_REGEXP
-
-# Whether the pattern highlighter should be called or not.
-_zsh_highlight_highlighter_regexp_predicate()
-{
-  _zsh_highlight_buffer_modified
-}
-
-# Pattern syntax highlighting function.
-_zsh_highlight_highlighter_regexp_paint()
-{
-  setopt localoptions extendedglob
-  local pattern
-  for pattern in ${(k)ZSH_HIGHLIGHT_REGEXP}; do
-    _zsh_highlight_regexp_highlighter_loop "$BUFFER" "$pattern"
-  done
-}
-
-_zsh_highlight_regexp_highlighter_loop()
-{
-  local buf="$1" pat="$2"
-  integer OFFSET=0
-  local MATCH; integer MBEGIN MEND
-  local -a match mbegin mend
-  while true; do
-    [[ "$buf" =~ "$pat" ]] || return;
-    region_highlight+=("$((MBEGIN - 1 + OFFSET)) $((MEND + OFFSET)) $ZSH_HIGHLIGHT_REGEXP[$pat], memo=zsh-syntax-highlighting")
-    buf="$buf[$(($MEND+1)),-1]"
-    OFFSET=$((MEND+OFFSET));
-  done
-}
+# Test that highlighting "doesnotexist" does not invoke the "rehash" builtin,
+# which would delete hashed commands (such as "zsyh-hashed-command").
+expected_region_highlight=(
+  "1 12 unknown-token"     # doesnotexist
+  "13 13 commandseparator" # ;
+  "15 33 hashed-command"   # zsyh-hashed-command
+)
