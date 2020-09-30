56cfa10ebf77f0056fe4e52c7981a2843f8cc587
Callan Bryant <callan.bryant@gmail.com>
2020-09-30 19:48:48 +0100

    add FREEBSD_AMD64 dependencies script

---
+++ system-dependencies/FREEBSD_AMD64.sh
@@ -0,0 +1,18 @@
+sudo pkg install -y \
+    bash-completion
+    curl \
+    fzf \
+    git \
+    gnupg2 \
+    htop \
+    ncdu \
+    ncdu \
+    ripgrep \
+    sudo \
+    tmpreaper \
+    tmux \
+    tree \
+    vim-console \
+    wget \
+    wireguard \
+    zsh \
