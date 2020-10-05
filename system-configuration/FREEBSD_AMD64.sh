84b87dbc5b60ca979b5dad58aeb299ecb59c9399
Callan Bryant <callan.bryant@gmail.com>
2020-10-05 20:47:39 +0100

    replace sshd config in openbsd

--- system-configuration/FREEBSD_AMD64.sh
+++ system-configuration/FREEBSD_AMD64.sh
@@ -3,4 +3,8 @@ cat <<-EOF | sudo tee /usr/local/etc/sudoers.d/wheel > /dev/null
     %wheel ALL=(ALL) ALL
 EOF
 
-echo FARGLEBUS
+# replace ssh config if password authentication is enabled (on by default)
+if ! sudo grep -q '^PasswordAuthentication no' /etc/ssh/sshd_config; then
+    sudo cp system-configuration/etc/sshd_config /etc/ssh/
+    sudo service sshd restart
+fi
--- system-configuration/RASPBIAN_ARMV5.sh
+++ system-configuration/RASPBIAN_ARMV5.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 
 # replace ssh config if password authentication is enabled (on by default)
-if ! sudo grep -q 'PasswordAuthentication no' /etc/ssh/sshd_config; then
+if ! sudo grep -q '^PasswordAuthentication no' /etc/ssh/sshd_config; then
     sudo cp system-configuration/etc/sshd_config /etc/ssh/
     sudo systemctl restart ssh
 fi
